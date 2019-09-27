b5d687662ac2e62a29f6e01b3f8d0ccd3b4bf0fa
Ying Zhou <yzhouyzhou@yahoo.com>
2019-09-27 14:38:25 -0500

    fix server warning

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lifechat");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lifechat",  {useNewUrlParser: true});
 
 // Start the API server
 app.listen(PORT, function() {
