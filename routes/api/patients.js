d0dd404fedabadead9e4c18b03bf33f8443eb093
alciancio <alcianciotto@gmail.com>
2019-09-29 20:33:29 -0500

    JSX to JS

--- routes/api/patients.js
+++ routes/api/patients.js
@@ -2,14 +2,13 @@ const router = require("express").Router();
 const patientsController = require("../../controllers/patientsController");
 
 // Matches with "/api/patients"
-router.route("/")
+router
+  .route("/")
   .post(patientsController.create)
   .get(patientsController.findOne);
 
-
-  // Matches with "/api/patients"
-router.route("/all")
-.get(patientsController.findAll);
+// Matches with "/api/patients"
+router.route("/all").get(patientsController.findAll);
 // Matches with "/api/patients/:id"
 router
   .route("/:id")
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-require('dotenv').config({ path: '.env' });
+require("dotenv").config({ path: ".env" });
 
 const express = require("express");
-const bodyParser = require('body-parser');
-    const cors = require('cors');
-    const Chatkit = require('@pusher/chatkit-server');
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const Chatkit = require("@pusher/chatkit-server");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
@@ -21,46 +21,48 @@ app.use(routes);
 
 const chatkit = new Chatkit.default({
   instanceLocator: process.env.CHATKIT_INSTANCE_LOCATOR,
-  key: process.env.CHATKIT_SECRET_KEY,
+  key: process.env.CHATKIT_SECRET_KEY
 });
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/users', (req, res) => {
-  console.log('user route hit')
+app.post("/users", (req, res) => {
+  console.log("user route hit");
   const { userId } = req.body;
 
   chatkit
     .createUser({
       id: userId,
-      name: userId,
+      name: userId
     })
     .then(() => {
       res.sendStatus(201);
     })
     .catch(err => {
-      if (err.error === 'services/chatkit/user_already_exists') {
+      if (err.error === "services/chatkit/user_already_exists") {
         console.log(`User already exists: ${userId}`);
         res.sendStatus(200);
       } else {
-        console.log('error is here')
+        console.log("error is here");
         res.status(err.status).json(err);
       }
     });
 });
 
-app.post('/authenticate', (req, res) => {
+app.post("/authenticate", (req, res) => {
   const authData = chatkit.authenticate({
-    userId: req.query.user_id,
+    userId: req.query.user_id
   });
   res.status(authData.status).send(authData.body);
 });
 
-
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lifechat",  {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lifechat", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 // mongoose.connect(process.env.MONGODB_URI || "mongodb://project3:project3@ds143245.mlab.com:43245/heroku_t6n4mrws",  {useNewUrlParser: true});
 
 // Start the API server
