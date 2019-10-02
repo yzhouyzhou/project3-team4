515bc3be7bb83147bd214d294fcc2993320575ab
alciancio <alcianciotto@gmail.com>
2019-10-02 00:02:27 -0500

    Minor

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -28,10 +28,10 @@ function Homepage() {
           <p>APIC (Assistance Processing Interface Compound) is an application interface for patients needing immediate care and utility services. Equipped with the latest technology, APIC allows users to connect directly to their Primary Providers' office and check in for emergency visits for your help and convenience. Cut the line, save the wait. We believe our community deserves the best Healthcare service available.</p>
           <h5>Explore Our Patient Features!</h5>
           <h6>Emergency</h6>
-          <p>Need to check-in at your appointment but don't want the wait? Check in at our Emergency services to connect directly with your healthcare provider through our EPIC API. Check in <a href="/liveChat">here!</a></p>
+          <p>Need to check-in at your appointment but don't want the wait? Check in at our Emergency services to connect directly with your healthcare provider through our EPIC API. Check in <a href="/emergency">here!</a></p>
           <p></p>
           <h6>Live Chat</h6>
-          <p>APIC's implementation team works side by side with you to make sure you receive quick and accessible service wherever you are. With our LiveChat feature, talk directly with live doctors to attend to your immediate needs and get quick and useful medical assistance. Designated 24/7 support at your fingertips. Click <a href="/liveChat">here</a> for help!</p>
+          <p>APIC's implementation team works side by side with you to make sure you receive quick and accessible service wherever you are. With our LiveChat feature, talk directly with live doctors to attend to your immediate needs and get quick and useful medical assistance. Designated 24/7 support at your fingertips. Click <a href="/liveChat">here</a> for help.</p>
           <h6>Location</h6>
           <p>Need a prescription? With our location feature get access to the local pharmacies near you. Go <a href="/location">here</a> to check the locations near you.</p>
           </div>
