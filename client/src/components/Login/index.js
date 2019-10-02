30d5afa428ed65b6acb16a40bb3f3c599af100bd
Ying Zhou <yzhouyzhou@yahoo.com>
2019-10-01 22:56:51 -0500

    add login info

--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import Button from "@material-ui/core/Button";
 import "./style.css";
 
-class PatientLogin extends Component {
+class Login extends Component {
   state = {
     patient: {},
     email: "",
@@ -18,6 +18,7 @@ class PatientLogin extends Component {
         console.log(res.data);
         this.setState({ patient: res.data });
         console.log("now", this.state.patient);
+        window.location.assign(`/home`);
       })
       .catch(err => console.log(err));
   };
@@ -39,85 +40,52 @@ class PatientLogin extends Component {
       <div
         className="signIn"
       >
-          <h2 id = "header">SIGN IN</h2>
-        <span className = "signUpLink">
+        <h2 id="header">SIGN IN</h2>
+        <span className="signUpLink">
           New to APIC? <a href="/register">Sign Up</a>
           {/* <i class="material-icons">face</i> */}
         </span>
         <form>
-        <div id = "email" className = "formInput">
-          <Input
-            value={this.state.email}
-            onChange={this.handleInputChange}
-            name="email"
-            placeholder="Email"
-          />
+          <div id="email" className="formInput">
+            <Input
+              value={this.state.email}
+              onChange={this.handleInputChange}
+              name="email"
+              placeholder="Email"
+            />
           </div>
-        <div className = "formInput">
-          <Input
-            value={this.state.password}
-            onChange={this.handleInputChange}
-            name="password"
-            type= "password"
-            placeholder="Password"
-          />
-          </div>
-          {/* make this check for same password */}
-          <div id = "password" className = "formInput">
-          <Input
-            // value={this.state.password}
-            // onChange={this.handleInputChange}
-            name="password"
-            type= "password"
-            placeholder="Confirm Password"
-          />
+          <div className="formInput">
+            <Input
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              name="password"
+              type="password"
+              placeholder="Password"
+            />
           </div>
           <FormBtn onClick={this.handleFormSubmit}
           >LOGIN</FormBtn>
         </form>
         <Link to="/home">
-            <Button variant="outlined" color="primary">
-              Go To Home
+          <Button variant="outlined" color="primary">
+            Go To Home
             </Button>
-          </Link>
-          <Link to="/patients">
-              <Button variant="outlined" color="primary">
-                Go To Register
+        </Link>
+        <Link to="/patients">
+          <Button variant="outlined" color="primary">
+            Go To Register
               </Button>
-            </Link>
-            <Link to="/patientLogin">
-              <Button variant="outlined" color="primary">
-                Go To Patient Login
+        </Link>
+        <Link to="/patientLogin">
+          <Button variant="outlined" color="primary">
+            Go To Patient Login
               </Button>
-            </Link>
+        </Link>
       </div>
     );
   }
 }
 
-export default PatientLogin;
 
-{
-  /* <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>Patient Information {this.state.patient.name}</h1>
-            </Jumbotron>
-            {this.state.patient ? (
-              <strong>
-                {this.state.patient._id}
-                {this.state.patient.name}  
-                {this.state.patient.password}  
-                {this.state.patient.email}  
-                {this.state.patient.ssn}  
-                {this.state.patient.primaryDrInfo}  
-                {this.state.patient.insuranceInfo}  
-                {this.state.patient.lifechathistory ? (this.state.patient.lifechathistory.length ?
-                  this.state.patient.lifechathistory.map(chat =>
-                    chat.chathistory + " ") : "")
-                  : ""}
-              </strong>
-            ) : (
-                <h3>No Patients to Display</h3>
-              )}
-          </Col> */
-}
+
+export default Login;
