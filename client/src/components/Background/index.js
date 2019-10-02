8e27887dfde6bcb781a5d1601806ada80c6f85c2
alciancio <alcianciotto@gmail.com>
2019-10-01 19:10:41 -0500

    Front Screen Edit

--- client/src/components/Background/index.js
+++ client/src/components/Background/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./style.css";
 import Background from './img.jpg';
+import APIC from './APIC.png';
 
 function Image() {   
     return (
       <div id = "background">
     <img src={Background} alt="background" id = "backgroundImage"/>
-    <h2 id = "picHeader">APIC</h2>
+    <img src={APIC} alt="APIC" id = "apicImage"/>
+    {/* <h2 id = "picHeader">APIC</h2> */}
     </div>
     )}
 
