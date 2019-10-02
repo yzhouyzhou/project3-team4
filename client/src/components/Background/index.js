3a53917c0e359f24dcb08cb4084e1bd245ae04f6
alciancio <alcianciotto@gmail.com>
2019-10-01 19:20:02 -0500

    Color Edit

--- client/src/components/Background/index.js
+++ client/src/components/Background/index.js
@@ -7,8 +7,8 @@ function Image() {
     return (
       <div id = "background">
     <img src={Background} alt="background" id = "backgroundImage"/>
-    <img src={APIC} alt="APIC" id = "apicImage"/>
-    {/* <h2 id = "picHeader">APIC</h2> */}
+    {/* <img src={APIC} alt="APIC" id = "apicImage"/> */}
+    <h2 id = "picHeader">APIC</h2>
     </div>
     )}
 
